2b339ed5bf2ca8ed5f0f9c7682eeaf8b60a9ef6e
kittyxcode <katherine.pino059@gmail.com>
2023-12-11 18:26:53 -0300

    creacion clase abstracta Player

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,24 @@
-public class Player {
+abstract class Player {
+    String name;//nombre jugadora
+
+    int[] guesses;//historias de intentos de adivinanza
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int[] getGuesses() {
+        return guesses;
+    }
+
+    public void setGuesses(int[] guesses) {
+        this.guesses = guesses;
+    }
+
+    abstract int makeGuess();
+
 }
